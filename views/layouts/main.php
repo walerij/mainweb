7b5541b018861d718238814ce8b8a1def580e4ee
Administrator <becwal@yandex.ru>
2017-12-10 22:39:36 +0300

    Стиль главной страницы

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,31 +28,36 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Время зарабатывать',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar-default navbar-fixed-top',
         ],
     ]);
+
+    $items=[
+
+        ['label' => 'Счет', 'url' => ['/site/index']],
+        ['label' => 'Выплаты', 'url' => ['/site/about']] ,
+
+
+        Yii::$app->user->isGuest ? (
+        ['label' => 'Вход', 'url' => ['/site/login']]
+        ) : (
+
+            '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                'Выход (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>'
+        )
+    ];
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
+        'items' => $items ,
     ]);
     NavBar::end();
     ?>
