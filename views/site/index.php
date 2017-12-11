9cb6b9431fae7e289f13dd6dad316c647a5ab7ba
walerij <becwal@yandex.ru>
2017-12-11 11:31:24 +0300

    footer

---
+++ views/site/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Время зарабатывать';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1><?php echo $this->title;?></h1>
+
+        <p class="lead">Сайт для тех, кто хочет зарабатывать и быть в тренде</p>
+
+       
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Зарегистрироваться на нашем сайте</h2>
+
+                <p>Процедура регистрации проста. Зарегистрируйтесь на нашем сайте</p>
+
+                <p><a class="btn btn-default" href="/site/join">Регистрация &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Получите счет</h2>
+
+                <p>После регистрации Вам будет создан персональный счет, где Вы сможете копить XMR с двух ресурсов инета.</p>
+
+                <p><a class="btn btn-default" href="/site/demo">Демо-счет &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Следите за снятием денег</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
